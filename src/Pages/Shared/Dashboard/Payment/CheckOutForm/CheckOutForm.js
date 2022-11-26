9ea7572aefd11b07ed2ffab8e0ba95cd3f2b856d
MohammadFahad1 <fahad4bangladesh@gmail.com>
2022-11-27 02:39:48 +0600

    Almost done implementing stripe

--- src/Pages/Shared/Dashboard/Payment/CheckOutForm/CheckOutForm.js
+++ src/Pages/Shared/Dashboard/Payment/CheckOutForm/CheckOutForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import {
     CardElement,
@@ -11,6 +11,17 @@ const CheckOutForm = ({ order }) => {
     const stripe = useStripe();
     const elements = useElements();
     const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState('')
+
+    useEffect(() => {
+        fetch('http://localhost:5000/create-payment-intent', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ price: order.sellingPrice })
+        })
+            .then(res => res.json())
+            .then(data => setClientSecret(data.clientSecret))
+    }, [order.sellingPrice])
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -40,7 +51,7 @@ const CheckOutForm = ({ order }) => {
         <>
             <form onSubmit={handleSubmit}>
                 <CardElement />
-                <button className='btn btn-primary mt-2' type="submit" disabled={!stripe || !elements}>
+                <button className='btn btn-primary mt-2' type="submit" disabled={!stripe || !elements || !clientSecret}>
                     Pay
                 </button>
             </form>
